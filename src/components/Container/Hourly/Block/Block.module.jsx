6a8331a89c684d9601040b8b26fc8f02a9660e22
Michael <nesterukmisha27@gmail.com>
2023-07-11 11:32:36 +0300

    http-support-fixed

--- src/components/Container/Hourly/Block/Block.module.jsx
+++ src/components/Container/Hourly/Block/Block.module.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import styles from "./Block.module.css";
-import { toCelsius } from "../../../../logic/logic";
+import React, { useState } from 'react'
+import styles from './Block.module.css'
+import { toCelsius } from '../../../../logic/logic'
 
 export const Block = ({ info }) => {
-    const [date, setDate] = useState(info.dt_txt.split(" ")[0])
-    const [id, setId] = useState(info.weather[0].icon)
-    const [temp, setTemp] = useState(toCelsius(info.main.temp))
-    return (
-			<div className={styles.block}>
-				<h6>{date} </h6>
-				<img src={`http://openweathermap.org/img/w/${id}.png`} alt='' />
-            <h5>{ temp }°C</h5>
-			</div>
-		)
+	const [date, setDate] = useState(info.dt_txt.split(' ')[0])
+	const [id, setId] = useState(info.weather[0].icon)
+	const [temp, setTemp] = useState(toCelsius(info.main.temp))
+	return (
+		<div className={styles.block}>
+			<h6>{date} </h6>
+			<img src={`https://openweathermap.org/img/w/${id}.png`} alt='' />
+			<h5>{temp}°C</h5>
+		</div>
+	)
 }
